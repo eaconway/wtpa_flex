b96eed3977d4b8d2be06dd6d07ef4a812507707c
cornelis <dirk.haupt@gmail.com>
2018-10-23 09:20:54 -0700

    add fromDate thruDate

--- frontend/src/components/map/script.js
+++ frontend/src/components/map/script.js
@@ -1,6 +1,10 @@
 /*global x*/
 /*global google*/
 /*global $*/
+
+// import * as eventsAPIUtil from '../util/events_api_util';
+const key = 'BHFjDGKYYBYl65HYDBYWgVEt2IV7Thnc';
+
 export const getLocation = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -14,12 +18,17 @@ export const showPosition = (position) => {
     x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 
     let latlon = position.coords.latitude + "," + position.coords.longitude;
+    // debugger
+
+    // eventsAPIUtil.searchCities(cityName, date, tomorrow);
+
+    // `https://app.ticketmaster.com/discovery/v2/events?apikey=${key}&startDateTime=${date}T00:00:01Z&endDateTime=${tomorrow}T00:00:00Z&city=${cityName}&countryCode=US`
+
+    // https://app.ticketmaster.com/discovery/v2/events.json?apikey=BHFjDGKYYBYl65HYDBYWgVEt2IV7Thnc&latlong=37.798756,-122.4014491&radius=1&startDateTime=2018-10-22T12:00:00Z&endDateTime=2018-10-23T12:00:00Z
 
     $.ajax({
       type: "GET",
-      url:
-        "https://app.ticketmaster.com/discovery/v2/events.json?apikey=BHFjDGKYYBYl65HYDBYWgVEt2IV7Thnc&latlong=" +
-        latlon,
+      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&latlong=${latlon}&radius=1&fromDate=2018-10-22&thruDate=2018-10-23&maxResults=25`,
       async: true,
       dataType: "json",
       success: function(json) {
@@ -55,6 +64,7 @@ export const showError = (error) => {
 
 
 export const showEvents = (json) => {
+    // debugger
     for (let i = 0; i < json.page.size; i++) {
         $("#events").append("<p>" + json._embedded.events[i].name + "</p>");
     }
