85a3a22312819227e49f45964d6e43cc2f16a700
Eric <eaconwayjr@gmail.com>
2018-10-25 09:04:41 -0700

    add messages

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const passport = require("passport");
+// const passport = require("passport");
 
-const Party = require("../../models/Party");
-const validatePartyInput = require("../../validations/parties");
+const Message = require("../../models/Message");
+// const validatePartyInput = require("../../validations/parties");
 
 router.get("/", (req, res) => {
-    Party.find()
-        .sort({ date: -1 })
-        .then(parties => {
+    Message.find()
+        .then(messages => {
             let results = {};
-            parties.forEach(party => {
-                results[party.id] = party
+            messages.forEach(message => {
+                results[message.id] = message
             });
             res.json(results);
         })
-        .catch(err => res.status(404).json({ nopartiesfound: "No parties found" }));
+        .catch(err => res.status(404).json({ nomessagesfound: "No messages found" }));
 });
+
+router.get("/deleteAll" , (req,res) => {
+    Message.deleteMany({ __v: 0})
+})
+
+module.exports = router;
