6c97aab05bd60c1e9a8e875f8993265e6f69f2a8
cornelis <dirk.haupt@gmail.com>
2018-10-27 13:47:03 -0700

    remove comments

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -19,11 +19,8 @@ class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploadedProfilePic: '', //imagePreviewUrl: '', online_url: '',
+      uploadedProfilePic: '', 
       uploadedFileCloudinaryUrl: '' };
-    // this._handleProfilePicChange = this._handleProfilePicChange.bind(this);
-    // this._handleSubmit = this._handleSubmit.bind(this);
-    // this.imageExists = this.imageExists.bind(this);
   }
 
   onImageDrop(files) {
@@ -64,126 +61,27 @@ class Profile extends React.Component {
     });
   }
 
-
-  // imageExists(url) {
-  //   var image = new Image();
-  //   image.src = url;
-  //   if (!image.complete) {
-  //     return false;
-  //   }
-  //   else if (image.height === 0) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
-
-  // _handleProfilePicChange(e) {
-  //   e.preventDefault();
-  //   // this.setState({ online_url: "" });
-  //   let reader = new FileReader();
-  //   let file = e.target.files[0];
-  //   let that = this;
-  //   reader.onloadend = () => {
-  //     this.setState({
-  //       file: file,
-  //       imagePreviewUrl: reader.result
-  //     });
-  //     that.props.updateUser({
-  //       id: that.props.currentUserId,
-  //       profilePicture: that.state.imagePreviewUrl
-  //     });
-  //   };
-
-  //   reader.readAsDataURL(file);
-  // }
-
-  // _handleSubmit(e) {
-  //   debugger
-  //   e.preventDefault();
-  //   // if (this.imageExists(this.state.online_url)) {
-  //   //   this.props.updateUser({ id: this.props.currentUser.id, image_url: this.state.online_url }).then(
-  //   //     this.eventFire(document.getElementById('cog'), 'click'));
-  //   // } else 
-  //   if (this.imageExists(this.state.imagePreviewUrl)) {
-  //     debugger
-  //     this.props.updateUser({ id: this.props.currentUser.id, profilePicture: this.state.imagePreviewUrl })
-  //   }
-  // }
-
   render() {
-    // let { imagePreviewUrl, online_url } = this.state;
     let profilePicture = null;
-    // let loading = <div className="avatar-previewText">Loading...</div>;
   
     if (this.state.uploadedFileCloudinaryUrl != '') {
       profilePicture = <img className="avatar-img" src={this.state.uploadedFileCloudinaryUrl} />;
     } 
-    // else if (online_url) { 
-    //   if (this.imageExists(online_url)) {
-    //     $imagePreview = (<img src={online_url} />);
-    //   } else {
-    //     $imagePreview = loading;
-    //   }
-    // } else {
-    //   $imagePreview = <div className="avatar-previewText">
-    //     </div>;
-    // }
-
-    //TODO: Cloudinary
-    ////////////////////////////////////////////////////////
-    //   <CloudinaryContext cloudName="wtpa">
-    //     <Image publicId="sample" format="jpg">
-    //       <Transformation crop="fill" gravity="faces" width="300" height="200" />
-    //     </Image>
-    //   </CloudinaryContext>
-
-    // var widget = cloudinary.createUploadWidget({
-    //   cloudName: "demo", uploadPreset: "preset1"
-    // }, (error, result) => { });
-    // widget.open();
-    /////////////////////////////////////////////////////////
-
-
-
-  // <Image publicId="lady.jpg" >
-  //   <Transformation width="400" height="400" gravity="face" radius="max" crop="crop" />
-  //   <Transformation width="200" crop="scale" />
-  // </Image> 
 
     return (
       <div>
         <div className="header-avatar">
           <div className="users-name">Testing</div>
-          {/* <form onSubmit={this._handleSubmit}> */}
             <div className="avatar-section">
               <div className="avatar-position" >
                 {profilePicture}
-                {/* <input className="avatar-fileInput"
-                  type="file"
-                  onChange={this._handleProfilePicChange} /> */}
               <Dropzone className='avatar-dropzone'
                 multiple={false}
                 accept="image/*"
                 onDrop={this.onImageDrop.bind(this)}>
               </Dropzone>
-              {/* <div className="FileUpload">
-                ...
-              </div> */}
-
-              {/* <div>
-                {this.state.uploadedFileCloudinaryUrl === '' ? null :
-                  <div>
-                    <img src={this.state.uploadedFileCloudinaryUrl} />
-                  </div>}
-              </div> */}
-
-
-              {/* <input name="file" type="file"
-                class="file-upload" data-cloudinary-field="image_id"
-                data-form-data="{ 'transformation': {'crop':'limit','tags':'samples','width':3000,'height':2000}}" /> */}
               </div>
             </div>
-          {/* </form> */}
           <div className="users-location">San Francisco, CA</div>
           <div className="users-name-field">
             <input type="text" placeholder="Name" />
@@ -201,21 +99,6 @@ class Profile extends React.Component {
             <textarea placeholder="Things I love" />
           </div>
         </div>
-
-        {/* <CloudinaryContext cloudName="demo">
-          <div>
-            <Image publicId="sample" width="50" />
-          </div>
-          <Image publicId="sample" width="0.5" />
-        </CloudinaryContext> */}
-
-        {/* <Image publicId="sample_woman.jpg" >
-          <Transformation width="400" height="350" gravity="face" crop="crop" />
-          <Transformation radius="20" border="5px_solid_black" />
-          <Transformation overlay="cloudinary_icon" opacity="50" width="0.25" flags="relative" gravity="north_east" y="10" x="10" />
-          <Transformation quality="auto" fetchFormat="auto" />
-        </Image> */}
-
       </div>
     );
   }
